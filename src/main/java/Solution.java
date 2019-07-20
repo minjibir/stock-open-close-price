5b9e39dc5d1bfd84e69d235f2027c2ffa6b5ba08
Jabir S. Minjibir <jminjibir@swglobal.com>
2019-07-20 20:03:53 +0100

    Checkpoint

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -4,18 +4,46 @@ import java.text.*;
 import java.math.*;
 import java.util.regex.*;
 import java.net.*;
+
 import com.google.gson.*;
 
+class Data {
+    private String date;
+    private double open;
+    private double close;
+    private double high;
+    private double low;
+}
+
+class StockInfo {
+    private int page;
+    private int perPage;
+    private int total;
+    private int totalPages;
+    private Data data;
+}
 
 public class Solution {
     /*
      * Complete the function below.
      */
     static void openAndClosePrices(String firstDate, String lastDate, String weekDay) {
+        final String STOCK_API_BASE_URL = "https://jsonmoc.hackerrank.com/api/stocks";
+        URL stockApi;
+        HttpURLConnection stockUrlkConnection;
 
+        try {
+            stockApi = new URL(STOCK_API_BASE_URL);
+            stockUrlkConnection = (HttpURLConnection) stockApi.openConnection();
+            stockUrlkConnection.setRequestMethod("GET");
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         String _firstDate;
         try {
